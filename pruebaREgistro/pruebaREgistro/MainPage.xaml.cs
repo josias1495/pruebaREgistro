a6ce914012eb981c31d2333f97cee7da7075f450
Jonathan Eliseo Marquez Fuentes <jonathan.marquez@sv.cds>
2019-10-11 15:20:51 -0600

    Agregar archivos de proyecto.

---
+++ pruebaREgistro/pruebaREgistro/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace pruebaREgistro
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Guardar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ pruebaREgistro/pruebaREgistro/RegistroMD.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pruebaREgistro
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RegistroMD : ContentPage
+	{
+		public RegistroMD ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Guardar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Guardar_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
